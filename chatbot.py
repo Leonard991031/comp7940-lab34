67fe90941de1950313f64934478f9f7068353682
Leonard991031 <xhy1031@yeah.net>
2022-02-17 10:21:11 +0800

    chatbotupdate5 commit

--- chatbot.py
+++ chatbot.py
@@ -29,7 +29,7 @@ def main():
     # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("add", add))
     dispatcher.add_handler(CommandHandler("help", help_command))
-    dispatcher.add_handler(CommandHandler("hello", help_command))
+    dispatcher.add_handler(CommandHandler("hello", hello))
 
 
     # To start the bot:
